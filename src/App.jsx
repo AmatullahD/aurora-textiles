26efe6836c82384c492c2983a32ec0a7d1e9c5b4
Amatullah <amatullahd99@gmail.com>
2026-05-16 18:02:14 +0530

    Fixed imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import { Routes, Route } from "react-router-dom";
 
-import Home from "./pages/home";
-import About from "./pages/about";
-import Contact from "./pages/contact";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
 
 function App() {
   return (
